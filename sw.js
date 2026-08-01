fd7c066cff44dcd3348417ff5b9d82b4f847a542
andremutalibov <91817056+andremutalibov@users.noreply.github.com>
2026-08-01 23:05:42 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Duit Kita — minimal shell cache */
-const CACHE = 'duit-kita-v28';
+const CACHE = 'duit-kita-v29';
 const SHELL = ['./index.html', './manifest.json', './icon-180.png', './icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting()));
